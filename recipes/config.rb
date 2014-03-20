788772eda753d7fc86cd0292231b1f78ceb01102
Campbell Allen <campbell.allen@gmail.com>
2014-03-20 08:53:35 +0000

    avoid shared config dead links to internal vm file locations in dev env

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,18 +1,22 @@
+prod_env = node.chef_environment == "production"
 template_vars = {}
 
 node['rails_app']['configuration']['vars'].each do |k, v|
   template_vars[k.to_sym] = v
 end
 
-directory "/rails/apps/#{node['rails_app']['name']}/shared/config" do
-  action :create
-  owner node['rails_app']['user']
-  group node['rails_app']['group']
-  mode "0775"
+if prod_env
+  directory "/rails/apps/#{node['rails_app']['name']}/shared/config" do
+    action :create
+    owner node['rails_app']['user']
+    group node['rails_app']['group']
+    mode "0775"
+  end
 end
 
 node['rails_app']['configuration']['files'].each do |f|
-  template_path = "/rails/apps/#{node['rails_app']['name']}/shared/config/#{f}.yml"
+  rails_config_root = prod_env ? "shared" : "current"
+  template_path = "/rails/apps/#{node['rails_app']['name']}/#{rails_config_root}/config/#{f}.yml"
   template_vars[:envs] = [ node['rails_app']['environment'] ] | %w(test development staging production)
   unless ::File.exist?(template_path)
     template template_path do
@@ -25,8 +29,10 @@ node['rails_app']['configuration']['files'].each do |f|
     end
   end
 
-  config_file_path = "/rails/apps/#{node['rails_app']['name']}/current/config/#{f}.yml"
-  execute "symlink_config" do
-    command "if [ ! -e \"#{config_file_path}\" ] ; then ln -s #{template_path} #{config_file_path} ; fi"
+  if prod_env
+    config_file_path = "/rails/apps/#{node['rails_app']['name']}/current/config/#{f}.yml"
+    execute "symlink_production_config_files" do
+      command "if [ ! -e \"#{config_file_path}\" ] ; then ln -s #{template_path} #{config_file_path} ; fi"
+    end
   end
 end
